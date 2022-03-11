4ef4a685403cd8e3d9e8a1195eb08c4354603e2f
Joshua Copeland <josh@remotedevforce.com>
2022-03-10 16:15:05 -0800

    Update for 7.4

--- install/osx/brew-install-php74-xdebug.sh
+++ install/osx/brew-install-php74-xdebug.sh
@@ -4,8 +4,8 @@ sleep 1
 
 # Meat and potatoes
 brew update
-brew install php71
-brew install php71-xdebug
+brew install php@7.4
+pecl install xdebug
 
 # Verify
 php -i | grep xdebug
---
+++ www/example/web/start-php-server-w-profiler.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+php -dxdebug.mode=develop,coverage,debug -S 127.0.0.1:8000
